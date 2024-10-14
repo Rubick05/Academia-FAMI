10f228682bb063f31ac9a57fd562efa399f01b6d
Miguel Rodriguez <josemiguelrodriguezpenia@gmail.com>
2024-10-14 00:26:25 -0400

    error con imagen en la pagina AboutUs

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,14 +1,11 @@
 // src/pages/AboutUs.tsx
 import React from 'react';
 import './AboutUs.css';
-import aboutImage from '../assets/images/about.jpg'; // Asegúrate de tener esta imagen
-
 const AboutUs: React.FC = () => {
   return (
     <section id="about-us" className="about-us-section">
       <h2>Sobre Nosotros</h2>
       <div className="about-content">
-        <img src={aboutImage} alt="Sobre la Academia FAMI" className="about-image" />
         <p>
           En la Academia de Danza FAMI, nos apasiona la danza y nos dedicamos a enseñar una variedad de estilos para todas las edades y niveles. Nuestro equipo de instructores profesionales está comprometido con tu crecimiento y desarrollo artístico.
         </p>
