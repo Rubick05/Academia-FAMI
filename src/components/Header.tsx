7674a08b3c129aabb355daa84b39bba6639b2617
Fabrizhio-virus <202002293@est.umss.edu>
2024-11-20 18:53:17 -0400

    Barra de Navegacion Completa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,36 @@
-// src/components/Header.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import logo from '../assets/logos/logo-fami.png'; // Asegúrate de tener el logo en esta ruta
 
 const Header: React.FC = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true); // Si se hace scroll, cambia la clase
+      } else {
+        setIsScrolled(false); // Si no hay scroll, barra transparente
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="logo-container">
-        <Link to="/">
-          <img src={logo} alt="Logo Academia FAMI" className="logo" />
-        </Link>
+        <Link to="/" className="logo-text">FAMI</Link>
       </div>
       <nav className="navigation">
         <Link to="/" className="header-button">Home</Link>
         <Link to="/about-us" className="header-button">About Us</Link>
-        <Link to="/inscribete" className="header-button">Inscribete</Link>
+        <Link to="/inscribete" className="header-button">Inscríbete</Link>
+        <Link to="/instructores" className="header-button">Instructores</Link>
         <Link to="/horarios" className="header-button">Horarios</Link>
       </nav>
     </header>
